324bd5c90a39ccfe2cd09742f42aa38fb280e594
xiaobai <1024630744@qq.com>
2019-03-12 17:13:55 +0800

    123

--- src/main.js
+++ src/main.js
@@ -3,21 +3,24 @@ import App from './App.vue'
 import router from './assets/js/router'
 import axios from 'axios'
 
-import rem from './assets/js/rem' //--------- rem
+import rem from './assets/js/rem'  //--------- rem
 import {post,fetch,patch,put} from './assets/js/http' //---------封装axios请求函数
 import api from './assets/js/api' //---------api 接口集引入
 import store from './assets/js/store'   //引入状态管理 store
 // import Base from './assets/js/baseFun.js'         //---------自定义的公共函数和公共请求方法
+import BaiduMap from 'vue-baidu-map'  //---------百度地图API
 
+// 注册百度地图全局组件
+Vue.use(BaiduMap, {
+  // ak 是在百度地图开发者平台申请的密钥 详见 http://lbsyun.baidu.com/apiconsole/key */
+  ak: '5ok1C0oNGXjAX9G6G2xSr9Zaxp5RpIZC'
+})
 //定义全局变量
 Vue.prototype.$post=post;   //----------post 请求
 Vue.prototype.$get=fetch;   //----------get 请求
 Vue.prototype.$patch=patch;   //----------patch 请求
 Vue.prototype.$put=put;   //----------put 请求
 Vue.prototype.api=api;   //----------api 请求地址集
-
-console.log(111111111)
-
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,55 @@
 <template>
-  <div class="home">
-
-  </div>
+    <div class="home">
+        <baidu-map
+                :center="center"
+                :zoom="zoom"
+                @ready="handler"
+                map-type="BMAP_HYBRID_MAP"
+                class="map-view">
+        </baidu-map>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
-export default {
-  name: 'home',
-  components: {
-  },
-  mounted() {
-    this.$get('/cgi-bin/user/cgi_personal_card',{uin:1024630744})
-    .then(res=>{
-      console.log(res)
-    })
-    .catch(r=>{
-      console.log(r)
-      console.log('err')
-    })
-
-  }
-}
+    export default {
+        name: 'home',
+        components: {},
+        data() {
+            return {
+                center: {lng: 0, lat: 0},
+                zoom: 0,
+                BMap:null,
+                map:null
+            }
+        },
+        methods: {
+            handler({BMap, map}) {
+                this.BMap = BMap
+                this.map = map
+                let _this = this
+                // console.log(BMap, map)
+                this.zoom = 19
+                let geolocation = new this.BMap.Geolocation();
+                geolocation.getCurrentPosition( r => {
+                    if(r.point){
+                        _this.center.lng = r.point.lng
+                        _this.center.lat = r.point.lat
+                        console.log('定位成功')
+                    }else{
+                        console.log('定位失败')
+                    }
+                })
+            }
+        }
+    }
 </script>
+
+<style scoped lang="less">
+    .map-view {
+        position: fixed;
+        left: 0;
+        top: 0;
+        right: 0;
+        bottom: 0;
+    }
+</style>
